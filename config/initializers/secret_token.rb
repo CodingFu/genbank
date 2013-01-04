ec6d1e1ea1799f3d33b6bdbd21947ac515ea3ccc
Leet Rocket <coding.fu@gmail.com>
2013-01-04 20:20:25 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bank::Application.config.secret_token = '73a800c8095c277ff12022abff7ee79d65a8e9cef8eabb5f6896d62ccbaf69f88c2bef667c3e9f310b228ff86570f7fd91a5db40a593f9640b5273ba141623ec'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Bank::Application.routes.draw do
+  devise_for :users
+
+  root :to => "home#index"
+end
